414703edab93110a585803feb51b6c77092548af
Brecht Hoflack <brecht.hoflack@gmail.com>
2011-06-19 21:01:20 +0200

    Deleted files that don't belong in this branch.

--- apps/mqhub/src/mqhub.hrl
+++
@@ -1,7 +0,0 @@
--define(PRINT(Var), io:format("DEBUG: ~p:~p - ~p~n~n ~p~n~n", [?MODULE, ?LINE, ??Var, Var])).
--define(sync(PrefList, Command, Master),
-        riak_core_vnode_master:sync_command(PrefList, Command, Master)).
-
--define(N, 3).
--define(R, 2).
--define(W, 2).
